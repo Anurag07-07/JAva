71847959f0d1b5a9e71ec17f9692bdff68d63a8c
Anurag07-07 <adityanv*@gmail.com>
2024-08-31 01:28:17 +0530

    first commit

---
+++ src/Message.java
@@ -0,0 +1,24 @@
+public class Message {
+    public static void main(String[] args) {
+        System.out.println("Good Morning");
+        System.out.println("Subscribe");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println();
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        System.out.println();
+        System.out.println("    *");
+        System.out.println("   **");
+        System.out.println("  ***");
+        System.out.println(" ****");
+        System.out.println("*****");
+
+    }
+}
